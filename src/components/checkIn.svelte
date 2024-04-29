1b369328cb91731f16aa2d220bca1cab219579b6
Sergio <ortiz.sergio0303@gmail.com>
2024-04-29 15:25:04 -0400

    feat(svelte): checkIn component followed by location component

---
+++ src/components/checkIn.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let location = '';
+
+	async function success({ coords: { latitude, longitude} }) {
+		const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}&zoom=17`);
+		const reverseGeocode = await response.json();
+
+		location = reverseGeocode.display_name;
+	};
+
+	onMount(() => {
+		navigator.geolocation.getCurrentPosition(success);
+	});
+
+	import Location from './location.svelte';
+
+	let checking;
+
+	function checkIn() {
+		checking = true;
+	}
+</script>
+
+{#if checking}
+	<Location location={location} />
+{:else}
+	<h1>Press the Check-In button below</h1>
+	<button on:click={checkIn}>Check-In</button>
+{/if}
---
+++ src/components/location.svelte
@@ -0,0 +1,6 @@
+<h1>Your location</h1>
+<span aria-busy="{!location}">{ location ? location : 'Reverse geocoding...' }</span>
+
+<script>
+	export let location;
+</script>
