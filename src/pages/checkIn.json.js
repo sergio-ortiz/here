80ee8f1f8b6d747e4c7f91cc6e29d63507fb9525
Sergio <ortiz.sergio0303@gmail.com>
2024-05-01 15:28:08 -0400

    fix(signIn): check in endpoint set cookie default false

--- src/pages/checkIn.json.js
+++ src/pages/checkIn.json.js
@@ -1,6 +1,12 @@
 export const prerender = false;
 
 export async function GET(context) {
+	const cookie = context.cookies.get('loggedIn');
+
+	if(!cookie) {
+		context.cookies.set('loggedIn', 'false', { httpOnly: true, sameSite: 'lax' });
+	}
+
 	return new Response(
 		JSON.stringify(context.cookies.get('loggedIn')),
 	);
--- src/pages/signin.json.js
+++ src/pages/signin.json.js
@@ -1,7 +1,7 @@
 export const prerender = false;
 
 export async function GET(context) {
-	context.cookies.set('loggedIn', 'true', {httpOnly: true, sameSite: 'lax' });
+	context.cookies.set('loggedIn', 'true', { httpOnly: true, sameSite: 'lax' });
 	
 	return new Response(
 		JSON.stringify(context.cookies.get('loggedIn')),
