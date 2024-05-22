73fcbb0f54ce5aae611dd695a4dc7f7d345ed5af
Sergio <ortiz.sergio0303@gmail.com>
2024-05-22 16:17:16 -0400

    feat(db): time type date default localtime

--- db/config.ts
+++ db/config.ts
@@ -1,10 +1,10 @@
-import { defineDb, defineTable, column, NOW } from 'astro:db';
+import { defineDb, defineTable, column, NOW, sql } from 'astro:db';
 
 // https://astro.build/db/config
 const CheckIn = defineTable({
   columns: {
     location: column.text(),
-    time: column.date({ default: NOW }),
+    time: column.date({ default: sql`(datetime('now', 'localtime'))` }),
   },
 });
 
