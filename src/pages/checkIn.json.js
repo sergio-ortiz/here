81f389acc4d9db18b2d138c74e2a5b294e12075d
Sergio <ortiz.sergio0303@gmail.com>
2024-05-23 13:34:47 -0400

    feat(app): remove logs

--- src/pages/checkIn.json.js
+++ src/pages/checkIn.json.js
@@ -6,7 +6,6 @@ export async function POST({ request }) {
 	const { location, timestamp } = await request.json();
 	
 	const time = new Date(timestamp);
-	console.log(time.toISOString());
 
 	const query = await db.insert(CheckIn).values({ location, time }).returning();
 	
